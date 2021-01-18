5687043b62799b396db8e529b7ed3e9623fcd340
Ubuntu <forte@agora-rtmp.qszek3gvcequhhw1ssqg4lazpe.cx.internal.cloudapp.net>
2021-01-18 19:33:26 +0000

    Add scripts to build and install

---
+++ setup.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+
+
+if [ "$#" -ne 2 ]; then
+	echo "Usage: $0 /path/to/this/repo /path/to/build/at " >&2
+	echo "e.g. ./setup.sh /home/forte/server_side_custom_video_source /home/forte" >&2
+	echo "e.g. ./setup.sh /home/ubuntu/server_side_custom_video_source /home/ubuntu" >&2
+  exit 1
+fi
+
+sudo apt update
+sudo apt install -y build-essential git libpcre3 libpcre3-dev zlib1g zlib1g-dev libssl-dev unzip
+sudo apt install -y libavcodec-dev libavformat-dev libavutil-dev libswscale-dev libx264-dev nasm libavfilter-dev libopus-dev
+
+cd $2
+
+if  [ -e "$2/custom-ngnix" ]; then
+  rm -rf "$2/custom-ngnix"
+fi
+if ! [ -e "$2/custom-ngnix" ]; then
+  echo "creating $2/custom-ngnix" 
+  mkdir "$2/custom-ngnix" 
+fi
+
+
+cd "$2/custom-ngnix" 
+git clone https://github.com/arut/nginx-rtmp-module.git
+git clone https://github.com/nginx/nginx.git
+
+cp -r $1 $2/custom-ngnix
+cp $1/sdk/agora_linux.zip $2/custom-ngnix
+unzip $2/custom-ngnix/agora_linux.zip
+cd $2/custom-ngnix/server_side_custom_video_source/libagorac
+sudo ./install.sh $2/custom-ngnix/Agora_Native_SDK_for_Linux_x64_rel.v2.7.1.1_2815_FULL_20201114_0418
+cd $2/custom-ngnix/server_side_custom_video_source
+cp ngx_agora_helper.c  ../nginx-rtmp-module/
+cp ngx_agora_helper.h  ../nginx-rtmp-module/
+cp patches/nginx.patch ../nginx
+cp patches/nginx-rtmp-module.patch ../nginx-rtmp-module/
+cd ../nginx
+patch -f -p 1 < nginx.patch
+cd ../nginx-rtmp-module/
+patch -f -p 1 < nginx-rtmp-module.patch
+cd ../nginx
+./auto/configure --add-module=../nginx-rtmp-module
+make
+sudo make install
+sudo cp $1/nginx.conf /usr/local/nginx/conf/nginx.conf
+echo "@reboot   $2/custom-ngnix/nginx/objs/nginx;"  | sudo crontab -
+
+
+
+echo "Clean up"
+rm -f "$2/custom-ngnix/agora_linux.zip"
+rm -rf "$2/custom-ngnix/server_side_custom_video_source"
+rm -rf "$2/custom-ngnix/nginx-rtmp-module"
