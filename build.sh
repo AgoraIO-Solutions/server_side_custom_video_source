30e75d31137e63caa899211e04f20efc53a76316
Ben Weekes <benweekes73@gmail.com>
2021-05-17 09:36:01 +0000

    updated binary package

--- build.sh
+++ build.sh
@@ -7,6 +7,7 @@ cd package
 cp /home/ubuntu/custom-ngnix/nginx/objs/nginx   source/usr/bin/
 cp /usr/local/lib/libagora_rtc_sdk.so source/usr/local/lib/
 cp /usr/local/lib/libagorac.so        source/usr/local/lib/
+cp ../rtmpg.conf bin/conf/rtmpg.conf
 dpkg-deb --build source bin/agora-rtmp-stream.deb
 #cp bin/agora-rtmp-stream.deb  package/bin/
 
